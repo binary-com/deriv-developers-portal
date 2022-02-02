161579df1bcdce21324b0c856ecdbad7bb5dffb1
Adam Rybinski <adam.rybinski@regentmarkets.com>
2022-02-02 14:57:53 +0100

    Revert app_id and server_url to localStorage

--- registration_api.js
+++ registration_api.js
@@ -318,14 +318,14 @@ const is_production = window.location.hostname === 'api.deriv.com';
 if (is_production) {
     const app_id = 31063;
     const server_url = 'https://green.binaryws.com';
-    sessionStorage.setItem('app_id', app_id);
-    sessionStorage.setItem('server_url', server_url);
+    localStorage.setItem('app_id', app_id);
+    localStorage.setItem('server_url', server_url);
 }
 
 const app_id_in_url = urlParams.get('app_id');
-if (app_id_in_url) sessionStorage.setItem('app_id', app_id_in_url);
+if (app_id_in_url) localStorage.setItem('app_id', app_id_in_url);
 const endpoint_in_url = urlParams.get('endpoint');
-if (endpoint_in_url) sessionStorage.setItem('server_url', endpoint_in_url);
+if (endpoint_in_url) localStorage.setItem('server_url', endpoint_in_url);
 
 const getToken = () => {
     if (token_input) {
@@ -337,9 +337,9 @@ const getStorageToken = () => {
     return sessionStorage.getItem('token1');
 }
 
-const getSessionAppId = () => sessionStorage.getItem("app_id");
+const getSessionAppId = () => localStorage.getItem("app_id");
 // add getEndpoint function
-const getEndpoint = () => sessionStorage.getItem('server_url');
+const getEndpoint = () => localStorage.getItem('server_url');
 
 
 
