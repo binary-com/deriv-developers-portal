af2bf5bae519b92646440e686993066d504c6095
adam-deriv <91898870+adam-deriv@users.noreply.github.com>
2022-03-09 18:13:58 +0100

    Add Improvements to Registration flow and Static (#51)

---
+++ combine_actors.js
@@ -0,0 +1,13 @@
+import {
+    createEffect,
+    createSignal,
+  } from "https://cdn.skypack.dev/solid-js";
+import { createMachine, interpret } from "https://unpkg.com/xstate@4/dist/xstate.web.js";
+
+const combined_actors = 
+  
+/** @xstate-layout N4IgpgJg5mDOIC5gF8A0IB2B7CdGgGMsBbAIwEsNIB9AQwIBcsAneJEABy1nIfKwz4QAD0QBGAEzoAnuInIFyIA */
+createMachine(
+    {
+  id: "combined_actors",
+});
