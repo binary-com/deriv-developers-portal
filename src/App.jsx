4324b1a32a027580a8df9c220092832896d13c08
Adam Rybinski <adam.rybinski@regentmarkets.com>
2022-03-10 16:14:46 +0100

    Add query provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 import logo from './logo.svg'
 import './App.css'
 import './stateSignal';
@@ -9,39 +11,43 @@ function App() {
     send('LOGIN');
   };
 
+  const queryClient = new QueryClient()
+
   return (
-    <div id="app" className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+    <QueryClientProvider client={queryClient}>
+      <div id="app" className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello Vite + React!</p>
           <button type="button" onClick={setOurCountFunction}>
             LOGIN
           </button>
           <div className='logged-in'>You are logged in!</div>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+          <p>
+            Edit <code>App.tsx</code> and save to test HMR updates.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {' | '}
+            <a
+              className="App-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+          </p>
+        </header>
+      </div>
+    </QueryClientProvider>
   )
 }
 
