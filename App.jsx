b99542038690ff0c8c4231babd9100f4a30ce75f
Adam Rybinski <adam.rybinski@regentmarkets.com>
2022-03-18 11:33:42 +0100

    Add Header image

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import './stateSignal';
 import { Router,Outlet} from '@tanstack/react-location';
 import { routes,location } from './Router';
 import { Suspense } from 'react';
-import MainNav from './components/MainNav';
+import Header from './components/Header/Header';
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <Suspense fallback={<div>loading</div>}>
       <QueryClientProvider client={queryClient}>
           <Router routes={routes} location={location}>
-            <MainNav />
+            <Header />
             <Outlet/>
           </Router>
       </QueryClientProvider>
