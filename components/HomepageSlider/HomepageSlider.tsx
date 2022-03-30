5954a2e1a4cb08aab9eecf8dfd91cb17b8017631
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2022-03-30 09:49:10 +0200

    homepage slider component

---
+++ components/HomepageSlider/HomepageSlider.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+
+export default function HomepageSlider() {
+    const authors = ["alessandro", "thiago", "josh"];
+    const [slide_position, setSlidePosition] = useState(0);
+    const [slide_data, setSlideData] = useState([]);
+
+    const slidePosition = (direction) => {
+        if (direction === "previous" && !(slide_position < 1)) {
+            setSlidePosition(slide_position - 1);
+        }
+        if (direction === "next" && !(slide_position > authors.length)) {
+            setSlidePosition(slide_position + 1);
+        }
+    }
+
+    const nextOrPrevSlide = (direction) => {
+        slidePosition(direction)
+        const current_slide = authors[slide_position];
+        let content_string;
+        let author_string;
+
+        switch(current_slide) {
+            case "alessandro":
+                content_string = "";
+                author_string = "";
+                break;
+            case "thiago":
+                content_string = "";
+                author_string = "";
+                break;
+            case "josh":
+                content_string = "";
+                author_string = "";
+                break;
+            default:
+                content_string = "";
+                author_string = "";
+        }
+        return setSlideData({content: content_string, author: author_string})
+
+        // assign class to slider component instead to determine in css how it slides.
+    }
+    
+    return (
+        <div id="slider" className="slider loaded">
+            <div className="wrapper">
+                <div id="slides" className="slides">
+                    <div className="slide">
+                        <blockquote className="content">
+                            To be honest, Deriv’s API is one of the best APIs in the trading market.
+                        </blockquote>
+                        <hr className="blockquote-separator" />
+                        <p className="blockquote-author"><b>Alessandro</b>, CEO | Italy</p>
+                    </div>
+                    <div className="slide">
+                        <blockquote className="content">
+                            Probably the best API for making your business successful in trading derivatives out there.
+                        </blockquote>
+                        <hr className="blockquote-separator" />
+                        <p className="blockquote-author"><b>Thiago</b>, entrepreneur | Brazil</p>
+                    </div>
+                    <div className="slide">
+                        <blockquote className="content">
+                            I have been using the Deriv API for 13 years to build successful apps in and I find the support I get from Deriv as a business partner second to none. I look forward to 13 more successful years to come.
+                        </blockquote>
+                        <hr className="blockquote-separator" />
+                        <p className="blockquote-author"><b>Josh</b>, trader | Australia</p>
+                    </div>
+                </div>
+            </div>
+            <div id="prev" className="control prev" />
+            <div id="next" className="control next" />
+        </div>
+    );
+}
