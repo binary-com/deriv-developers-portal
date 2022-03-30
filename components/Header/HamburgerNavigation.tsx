6a9bd91e15ac5959de30645578c895c74d89130e
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2022-03-30 11:10:24 +0200

    small link fix

--- components/Header/HamburgerNavigation.tsx
+++ components/Header/HamburgerNavigation.tsx
@@ -13,12 +13,12 @@ export default function HamburgerNavigation() {
                     <button onClick={() => send('TOGGLE_DOCUMENTATION') }>Documentation</button>
                     <section className={styles.documentationSection}>
                         <li><Link to="/docs/">Quickstart</Link></li>
-                        <li><Link to="/docs/app-registration">App Registration</Link></li>
-                        <li><Link to="/api-explorer">API Explorer</Link></li>
-                        <li><Link to="/docs/api-guide">API Guide</Link></li>
-                        <li><Link to="/docs/faq">FAQ</Link></li>
-                        <li><Link to="/docs/json-schemas">JSON Schemas</Link></li>
-                        <li><Link to="/docs/bug-bounty">Bug Bounty</Link></li>
+                        <li><Link to="/docs/app-registration/">App Registration</Link></li>
+                        <li><Link to="/api-explorer/">API Explorer</Link></li>
+                        <li><Link to="/docs/api-guide/">API Guide</Link></li>
+                        <li><Link to="/docs/faq/">FAQ</Link></li>
+                        <li><Link to="/docs/json-schemas/">JSON Schemas</Link></li>
+                        <li><Link to="/docs/bug-bounty/">Bug Bounty</Link></li>
                     </section>
                 </ul>
             </nav>
