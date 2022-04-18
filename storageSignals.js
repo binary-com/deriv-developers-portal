1aebca4437604a1d3ddbf0da0f428a592ba16b3a
Adam Rybinski <adam.rybinski@regentmarkets.com>
2022-04-18 12:35:12 +0200

    move token to sessionStorage

--- storageSignals.js
+++ storageSignals.js
@@ -11,9 +11,9 @@ createEffect(() => {
 
     if (app_id_in_url) localStorage.setItem('app_id', app_id_in_url);
     if (endpoint_in_url) localStorage.setItem('server_url', endpoint_in_url);
-    if (token1_in_url) localStorage.setItem('token1', token1_in_url);
+    if (token1_in_url) sessionStorage.setItem('token1', token1_in_url);
     
-    const token1 = localStorage.getItem('token1');
+    const token1 = sessionStorage.getItem('token1');
     const app_id_from_storage = localStorage.getItem('app_id');
     const server_url_from_storage = localStorage.getItem('server_url');
     setToken1(token1);
