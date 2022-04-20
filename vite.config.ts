1315db8b06e3be10238eec3cbd575c81a521c2c0
Adam Rybinski <adam.rybinski@regentmarkets.com>
2022-04-20 12:00:31 +0200

    Add generated favicons

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    ViteFaviconsPlugin('./logo.png')
+    react()
   ]
 })
