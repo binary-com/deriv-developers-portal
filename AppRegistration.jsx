073e6192a191bdf92c98cfeba5d92075b5673b4d
adam-deriv <91898870+adam-deriv@users.noreply.github.com>
2022-04-20 09:54:02 +0200

    Display registered tabs in table (#75)

--- AppRegistration.jsx
+++ AppRegistration.jsx
@@ -1,3 +1,4 @@
+import AppManagement from './AppManagement';
 import './AppRegistration.scss';
 import { oauthUrl } from './appRegistrationEffects';
 import { send } from './stateSignal';
@@ -166,77 +167,7 @@ export default function AppRegistration() {
             </div>
           </dialog>
         </div>
-        <div className="manage-apps">
-          <div className="manage-apps-table">
-            <table>
-              <thead>
-                <tr>
-                  <th>Users</th>
-                  <th>Application ID</th>
-                  <th>Scopes</th>
-                  <th>Redirect URL</th>
-                  <th>
-                    {/* update app action column */}
-                  </th>
-                  <th>
-                    {/* remove app action column */}
-                  </th>
-                </tr>
-              </thead>
-              <tbody id="app_list">
-                {/* populate table with data from API */}
-              </tbody>
-            </table>
-            {/* display message "To modify your details please go back to the form and update your applicaton." when data-state empty */}
-            <div className="no-apps-wrapper">
-              <div className="no-apps">
-                <div className="no-apps-icon">
-                  <img src="/img/table-empty.svg" alt="No apps registered" />
-                </div>
-                <div className="no-apps-text">
-                  <p>To see your details reflected, please register your app via the registration form.</p>
-                </div>
-                {/* add go back button like in dialogs "got it" */}
-                <button className="gray-btn-submit" id="empty_go_back">Register now</button>
-              </div>
-            </div>
-          </div>
-          <dialog id="delete_app_dialog" className="dialog-wrapper">
-            <div className="dialog dialog-delete">
-              <div className="dialog-header">
-                <img id="close_delete_dialog" src="/img/close_dialog.svg" alt="Close" />
-              </div>
-              <div className="dialog-body dialog-body-delete">
-                <img className="dialog-warning" src="/img/warning.png" alt="Warning!" />
-                {/* Add "Delete app" bold text in the center */}
-                <h3>Delete app</h3>
-                <div className="dialog-question">Are you sure you want to delete this app?</div>
-              </div>
-              <div className="dialog-footer">
-                <button className="dialog-btn-cancel" id="delete_app_keep">No, keep it</button>
-                <button className="dialog-btn-submit" id="delete_app_button">Yes, delete</button>
-              </div>
-            </div>
-          </dialog>
-          {/* create a modal with a svg, header and text.  */}
-          <dialog id="register_app_dialog" className="dialog-wrapper">
-            <div className="dialog dialog-modal">
-              <div className="dialog-header">
-                <img id="close_register_app_dialog" src="/img/close_dialog.svg" alt="Close" />
-              </div>
-              <div className="dialog-body dialog-body-modal">
-                <img className="dialog-warning" src="/img/success.svg" alt="Success!" />
-                <h3>Success!</h3>
-                <div className="dialog-question">You have successfully registered your application. You can now start
-                  using Deriv API.</div>
-              </div>
-              <div className="dialog-footer">
-                <button className="dialog-btn-cancel" id="modal_got_it">Got it</button>
-                <button className="dialog-btn-submit" id="modal_app_register">Manage application</button>
-              </div>
-            </div>
-          </dialog>
-        </div>
+        <AppManagement />
       </div>
       <button id="logout" className="app-registration-signout gray-btn-submit">Sign out</button>
     </div>
