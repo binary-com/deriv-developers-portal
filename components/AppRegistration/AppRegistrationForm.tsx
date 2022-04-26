1b4ff56ad709f4c53c0e6b5b8103082a8a4c3998
hubert-deriv <80688204+hubert-deriv@users.noreply.github.com>
2022-04-26 17:01:17 +0200

    Hubert / React form hooks implementation (#79)

--- components/AppRegistration/AppRegistrationForm.tsx
+++ components/AppRegistration/AppRegistrationForm.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import { useForm } from 'react-hook-form';
+import Checkbox from '../Checkbox/Checkbox';
+import Button from '../Button/Button';
 import styles from './AppRegistrationForm.module.scss';
 
-type FormData = {
+interface FormData {
     api_token_input: string;
     app_name: string;
     app_markup_percentage: number;
@@ -11,20 +14,24 @@ type FormData = {
     trade_scope: string;
     trading_information_scope: string;
     payments_scope: string;
+    admin_scope: string;
 }
 
 export default function AppRegistrationForm () {
+    const [is_expanded, setIsExpanded] = React.useState(false);
+    const toggleFormExpand = () => is_expanded ? setIsExpanded(false) : setIsExpanded(true);
+
     const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
 
     return (
-        <form id="frmNewApplication" onSubmit={handleSubmit((data) => {console.log(data)} )}>
+        <form className={styles.frmNewApplication} id="frmNewApplication" onSubmit={handleSubmit((data) => {console.log(data)} )}>
             <div className={styles.formContent}>
                 <fieldset>
                     <div className={styles.formHeaderContainer}>
                         <h4 className={styles.registerFormHeader}>General information</h4>
                         <div className={styles.infoIcon}>
                             <div className={styles.tooltip}>
-                                Please create your API token
+                                Please create your API token {" "}
                                 <a href="https://app.deriv.com/account/api-token" className={styles.tooltipLink}>here</a>, and
                                 copy it into this field.
                             </div>
@@ -33,101 +40,205 @@ export default function AppRegistrationForm () {
                     </div>
                     <div className="api-token-wrapper">
                         <div className={styles.customTextInput} id="custom-text-input">
-                            <input {...register("api_token_input", { required: true })} type="text" id="api_token_input" className={styles.apiTokenInput} placeholder=" " />
-                            {errors.api_token_input && <span className={styles.errorMessage}>API token is required. wat dis?</span>}
+                            <input {...register(
+                                "api_token_input", {
+                                    required: {
+                                        value: true,
+                                        message: "You require an API token to register an app."
+                                    },
+                                    maxLength: {
+                                        value: 255,
+                                        message: "You cannot write more than 255 characters."
+                                    }
+                                })}
+                                type="text"
+                                id="api_token_input"
+                                className={styles.apiTokenInput}
+                                placeholder=" "
+                            />
                             <label>API token (Required)</label>
                         </div>
+                        <p className={styles.helperText}>max. 50 characters</p>
+                        {errors.api_token_input && <span className="error-message">{errors.api_token_input.message}</span>}
                         <div className="api-token-warning" />
                         <div className="first">
                             <div className={styles.customTextInput} id="custom-text-input">
-                                <input {...register("app_name")} type="text" id="app_name" placeholder=" " />
+                                <input {...register(
+                                    "app_name", {
+                                        required: {
+                                            value: true,
+                                            message: "An app name is required.",
+                                        },
+                                        maxLength: {
+                                            value: 48,
+                                            message: "Your app name cannot exceed more than 48 characters.",
+                                        }
+                                    })} 
+                                    type="text"
+                                    id="app_name"
+                                    placeholder=" "
+                                />
                                 <label>App name (Required)</label>
                             </div>
+                            {errors.app_name && <span className="error-message">{errors.app_name.message}</span>}
                         </div>
                     </div>
                 </fieldset>
                 <div className={styles.expandForm}>
-                    <input id="expand_form" className={styles.expandFormCheckbox} type="checkbox" autoComplete="off" />
-                    <label htmlFor="admin-scope">I'd like to use OAuth or monetise my app</label>
+                    <Checkbox
+                        id="expand-form"
+                        label="I'd like to use OAuth or monetise my app"
+                        value=""
+                        onClickFunction={() => toggleFormExpand()}
+                    />
                 </div>
-                <div className={styles.expandableForm}>
+                <div style={{ display: is_expanded ? "inline-block" : "none" }} className={styles.expandableForm}>
                     <fieldset>
-                        <div className="form-header-container">
-                            <h4 className="register-form-header">Markup</h4>
-                            <div className="info-icon">
-                                <div className="tooltip">For each trade performed on your app, you will receive a
+                        <div className={styles.formHeaderContainer}>
+                            <h4 className={styles.registerFormHeader}>Markup</h4>
+                            <div className={styles.infoIcon}>
+                                <div className={styles.tooltip}>For each trade performed on your app, you will receive a
                                 commission. Set a markup percentage below to determine the commission you will
                                 make.</div>
-                                <img src="/img/info-icon.svg" alt="Info" />
+                                <div className={styles.infoIconImage} />
                             </div>
                         </div>
                         <div className="input-container">
                             <div>
-                                <div className="custom-text-input" id="custom-text-input">
-                                    <input {...register("app_markup_percentage", { required: true, valueAsNumber: true, maxLength: { value: 4, message:" Too Many Characters"} })} max="4" type="number" id="app_markup_percentage" className="last" placeholder=" " />
+                                <div className={styles.customTextInput} id="custom-text-input">
+                                    <input {...register(
+                                        "app_markup_percentage", {
+                                            pattern: {
+                                                value: /^((([0-4]\.([0-9]([0-9])?)?))||([5]\.([0]([0])?)?)||([0-5]))$/,
+                                                message: "Please choose a markup value between 0.00 and 5.00",
+                                            },
+                                            maxLength: {
+                                                value: 4,
+                                                message: "Markup cannot exceed more than 4 characters.",
+                                            }
+                                        })} 
+                                        type="number"
+                                        id="app_markup_percentage"
+                                        className="last"
+                                        placeholder=" "
+                                    />
                                     <label>Markup percentage</label>
                                 </div>
-                                <p className="helper-text">(0.00-5.00%)</p>
+                                <p className={styles.helperText}>(0.00-5.00%)</p>
+                                {errors.app_markup_percentage && <span className="error-message">{errors.app_markup_percentage.message}</span>}
                             </div>
                         </div>
-                        <div className="form-header-container">
-                            <h4 className="register-form-header">Authorisation</h4>
-                            <div className="info-icon">
-                                <div className="tooltip">To use OAuth, please fill out the following fields. These
+                        <div className={styles.formHeaderContainer}>
+                            <h4 className={styles.registerFormHeader}>Authorisation</h4>
+                            <div className={styles.infoIcon}>
+                                <div className={styles.tooltip}>To use OAuth, please fill out the following fields. These
                                 details can be changed later using the app_update API call.</div>
-                                <img src="/img/info-icon.svg" alt="Info" />
+                                <div className={styles.infoIconImage} />
                             </div>
                         </div>
                         <div className="input-container">
-                            <div className="custom-text-input" id="custom-text-input">
-                                <input {...register("app_redirect_uri")} id="app_redirect_uri" type="text" placeholder=" " />
+                            <div className={styles.customTextInput} id="custom-text-input">
+                                <input {...register(
+                                    "app_redirect_uri", { 
+                                        maxLength: {
+                                            value: 255,
+                                            message: "Your website URL cannot exceed more than 255 characters."
+                                        },
+                                        pattern: {
+                                            value: /^[a-z][a-z0-9.+\-]*:\/\/[0-9a-zA-Z\.-]+[\%\/\w \.-]*$/,
+                                            message: "Please correct your link formatting. (example: https://www.deriv.com)"
+                                        }
+                                    })}
+                                    id="app_redirect_uri"
+                                    type="text"
+                                    placeholder=" "
+                                />
                                 <label>Website URL</label>
                             </div>
-                            <p className="helper-text">*Please note that this URL will be used as the OAuth redirect
+                            <p className={styles.helperText}>*Please note that this URL will be used as the OAuth redirect
                                 URL for the OAuth authorisation</p>
+                            {errors.app_redirect_uri && <span className="error-message">{errors.app_redirect_uri.message}</span>}
                         </div>
                         <div className="input-container">
-                            <div className="custom-text-input" id="custom-text-input">
-                                <input {...register("app_verification_uri")} id="app_verification_uri" type="text" placeholder=" " />
+                            <div className={styles.customTextInput} id="custom-text-input">
+                                <input {...register(
+                                    "app_verification_uri", {
+                                        maxLength: {
+                                            value: 255,
+                                            message: "Your verification URL cannot exceed more than 255 characters."
+                                        },
+                                        pattern: {
+                                            value: /^[a-z][a-z0-9.+\-]*:\/\/[0-9a-zA-Z\.-]+[\%\/\w \.-]*$/,
+                                            message: "Please correct your link formatting. (example: https://www.deriv.com)"
+                                        }
+                                    })}
+                                    id="app_verification_uri"
+                                    type="text"
+                                    placeholder=" "
+                                />
                                 <label>Verification URL</label>
                             </div>
+                            {errors.app_verification_uri && <span className="error-message">{errors.app_verification_uri.message}</span>}
                         </div>
                     </fieldset>
-                    <div className="scopes" id="register_scopes">
+                    <div className={styles.scopes} id="register_scopes">
                         <div>
-                            <div className="form-header-container">
-                                <h4 className="register-form-header">OAuth authorisation levels</h4>
-                                <div className="info-icon">
-                                <div className="tooltip">Please select the level of access you would like clients to
-                                    give to your app.</div>
-                                <img src="/img/info-icon.svg" alt="Info" />
+                            <div className={styles.formHeaderContainer}>
+                                <h4 className={styles.registerFormHeader}>OAuth authorisation levels</h4>
+                                <div className={styles.infoIcon}>
+                                    <div className={styles.tooltip}>Please select the level of access you would like clients to
+                                        give to your app.</div>
+                                    <div className={styles.infoIconImage} />
                                 </div>
                             </div>
-                            <p>Bear in mind that you generally need only
-                                <b>'Trade'</b> and
+                            <p>Bear in mind that you generally need only {" "}
+                                <b>'Trade'</b> and {" "}
                                 <b>'Trading information'</b> access.
                             </p>
                         </div>
-                        <div className="scopes-field">
-                            <input {...register("read_scope")} id="read-scope" type="checkbox" defaultValue="read" />
-                            <label htmlFor="read-scope">Read all: Full access to users’ information, including private
-                                information</label>
+                        <div className={styles.scopesField}>
+                            <Checkbox 
+                                name={register("read_scope")} 
+                                id="read-scope"
+                                label="Read all: Full access to users' information, including private information"
+                                value="read"
+                            />
                         </div>
-                        <div className="scopes-field">
-                            <input {...register("trade_scope")} id="trade-scope" type="checkbox" defaultValue="trade" />
-                            <label htmlFor="trade-scope">Trade: Buy and sell contracts on the users’ behalf</label>
+                        <div className={styles.scopesField}>
+                            <Checkbox 
+                                name={register("trade_scope")}
+                                id="trade-scope"
+                                label="Trade: Buy and sell contracts on the users' behalf"
+                                value="trade"
+                            />
                         </div>
-                        <div className="scopes-field">
-                            <input {...register("trading_information_scope")} id="trading_information-scope" type="checkbox" defaultValue="trading_information" />
-                            <label htmlFor="trading_information-scope">Trading information: View users’ trading
-                                information, including balance information</label>
+                        <div className={styles.scopesField}>
+                            <Checkbox 
+                                name={register("trading_information_scope")}
+                                id="trading_information-scope"
+                                label="Trading information: View users' trading information, including balance information"
+                                value="trading_information"
+                            />
                         </div>
-                        <div className="scopes-field mb-0">
-                            <input {...register("payments_scope")} id="payments-scope" type="checkbox" defaultValue="payments" />
-                            <label htmlFor="payments-scope">Payments: Cashier (deposit and withdrawal)</label>
+                        <div className={styles.scopesField}>
+                            <Checkbox 
+                                name={register("payments_scope")}
+                                id="payments-scope"
+                                label="Payments: Cashier (deposit and withdrawal)"
+                                value="payments"
+                            />
+                        </div>
+                        <div className={`${styles.scopesField} mb-0`}>
+                            <Checkbox 
+                                name={register("admin_scope")} 
+                                id="admin-scope"
+                                label="Admin: Full account access, including the access to manage security tokens"
+                                value="admin"
+                            />
                         </div>
                     </div>
-                    <div className="terms-of-conditions-register">
+                    {errors.admin_scope && <span className="error-message">{errors.admin_scope.message}</span>}
+                    <div className={styles.termsOfConditionRegister}>
                         <span>By registering your application, you acknowledge that you’ve read and accepted the
                         Deriv API </span>
                         <a href="https://deriv.com/tnc/business-partners-api-user.pdf" target="_blank" rel="noreferrer">
@@ -135,7 +246,9 @@ export default function AppRegistrationForm () {
                         </a>
                         <span>.</span>
                     </div>
-                    <input type="submit" value="Register new application" className={`${styles.registerAppButton} primary-btn-submit`} id="btnRegister" />
+                    <div className={styles.registerAppButtonContainer}>
+                        <Button onClick={null}>Register new Application</Button>
+                    </div>
                 </div>
             </div>
             <input type="hidden" id="app_id" name="app_id" />
