df1fda2f22859738b91143daed7c3c88da9dfe17
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2022-05-18 12:47:34 +0200

    refactor

--- components/Header/HamburgerLink.tsx
+++ components/Header/HamburgerLink.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { send } from "../../stateSignal";
 import styles from "./Header.module.scss";
@@ -8,10 +9,20 @@ interface LinkProps {
 }
 
 export default function HamburgerLink({ location, name }:LinkProps) {
-    const is_current_location = window.location.pathname === location;
+    const [is_on_current_location, setIsOnCurrentLocation] = useState(false);
+    const location_no_slash = location.substring(0, location.length - 1);
+    useEffect(() => {
+        const is_valid_pathname = window.location.pathname === location || window.location.pathname === location_no_slash;
+        if (is_valid_pathname) {
+            setIsOnCurrentLocation(true);
+        } else {
+            setIsOnCurrentLocation(false);
+        }
+    }, [window.location.pathname])
+
     return ( 
         <Link 
-            className={is_current_location ? styles.selected : ""}
+            className={is_on_current_location ? styles.selected : ""}
             onClick={() => send('TOGGLE_HAMBURGER') }
             to={location}
         >
