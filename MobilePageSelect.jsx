ecbd100a4beefb472e2920066f94309a1f24da47
adam-deriv <91898870+adam-deriv@users.noreply.github.com>
2022-05-19 09:27:51 +0200

    Adjust mobile selector to new router (#78)

--- MobilePageSelect.jsx
+++ MobilePageSelect.jsx
@@ -8,14 +8,14 @@ export default function MobilePageSelect() {
     const onSelectChange = (event) => {
         const selectedPage = event.target.value;
         if (selectedPage) {
-            navigate({ to: selectedPage, replace: true })
+            navigate(selectedPage)
         }
     };
     return (
         <select className={styles.mobilePageSelect} onChange={onSelectChange} value={currentPath}>
             <option value="/docs/">Quickstart</option>
             <option value="/docs/app-registration/">App Registration</option>
-            <option value="/playground/">API Playground</option>
+            <option value="/docs/api-explorer/">API Playground</option>
             <option value="/docs/api-guide/">API Guide</option>
             <option value="/docs/faq/">FAQ</option>
             <option value="/docs/json-schemas/">JSON Schemas</option>
