ba5c1dcbd4a0e6cbd78acdcf04d45bb6614a165a
hubert-deriv <80688204+hubert-deriv@users.noreply.github.com>
2022-06-01 10:36:33 +0200

    Hubert / styling schema object (#123)

--- SchemaBody.tsx
+++ SchemaBody.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import React from "react";
 import styles from "./Schema.module.scss";
+import SchemaObjectContent from "./SchemaObjectContent";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type SchemaBodyProps = {
@@ -30,7 +31,7 @@ const SchemaBody: React.FC<SchemaBodyProps> = ({ properties }) => {
 export default SchemaBody;
 
 const CodeString: React.FC<CodeStringProps> = ({ description }) => {
-    const highlightCode = description.split(" ").map((desc, index) => {
+    const highlightCode = description?.split(" ").map((desc, index) => {
       const regex = /`([a-zA-Z_]*[a-zA-Z]_?)`/g;
         return (regex.test(desc)) ?
             <div key={`${index}-code`}>
@@ -47,37 +48,60 @@ const CodeString: React.FC<CodeStringProps> = ({ description }) => {
     );
 }
 
-const RecursiveProperties = ( { properties, value } : { properties: any, value: any}): any => {
+const RecursiveProperties = ( { properties, value } : { properties: any, value: any }): any => {
     const keys = properties && Object.keys(properties);
     if (!keys) {
         return <>
             <CodeString description={value.description}/>
             { value.description }
         </>
-
     }
     return keys?.map((key, index) => {
-        const { type, description, pattern, enum: _enum } = properties[key];
-        const value = properties[key];
+        const { type, items, description, pattern, enum: _enum } = properties[key];
+        // const nested_keys = items?.properties ? Object.keys(items);
+        // const {  } = items[nested_keys];
         return (
             <div className={styles.schemaBodySignature} key={`${index}-signature`}>
-                  <div className={styles.schemaBodyHeader}>
-                      <p><strong>{key}</strong></p>
-                      {_enum && <div className={styles.schemaBodyType}>{type}
-                          <div className={styles.enumFlex}>{_enum.map((el: string, i: number) => <div
-                              className={`${styles.schemaType} ${styles.schemaCode} ${styles.schemaEnums}`}
-                              key={i}>{el}</div>)}
-                          </div>
-                      </div> }
-                      {pattern && <div className={styles.schemaRegexContainer}>
-                          <div className={styles.schemaPatternType}>{type}</div>
-                          <div className={styles.schemaBodyPattern}>{pattern}</div>
-                      </div>}
-                      { type === "object" && <RecursiveProperties properties={value.properties} value={value}/> }
-                  </div>
-                { type !== "object" && <CodeString description={description}/> }
-              </div>
-            );
+                <div className={`${styles.schemaBodyHeader}${type === "object" ? ` ${styles.schemaObjectHeader}` : ''}`}>
+                    { type === "object" ?
+                        <SchemaObjectContent
+                            RecursiveProperties={RecursiveProperties}
+                            properties={properties}
+                            key_value={key}
+                        />
+                        :
+                        <p><strong>{key}</strong></p>
+                    }
+                    {/* { type === "array" &&
+                        <SchemaObjectContent
+                            RecursiveProperties={RecursiveProperties}
+                            properties={items.properties}
+                            key_value={key}
+                        />
+                    } */}
+                    { _enum && 
+                        <div className={styles.schemaBodyType}>
+                            {type}
+                            <div className={styles.enumFlex}>{_enum.map((el: string, i: number) => 
+                                <div
+                                    className={`${styles.schemaType} ${styles.schemaCode} ${styles.schemaEnums}`}
+                                    key={i}
+                                >
+                                    {el}
+                                </div>)}
+                            </div>
+                        </div> 
+                    }
+                    { pattern && 
+                        <div className={styles.schemaRegexContainer}>
+                            <div className={styles.schemaPatternType}>{type}</div>
+                            <div className={styles.schemaBodyPattern}>{pattern}</div>
+                        </div>
+                    }
+                </div>
+            { type !== "object" && <CodeString description={description}/> }
+            </div>
+        );
     });
 }
 
---
+++ SchemaObjectContent.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import styles from "./Schema.module.scss";
+import { isUpdateModeSelector } from './selectors';
+
+export default function SchemaObjectContent({RecursiveProperties, key_value, properties}) {
+    const [is_open_object, setIsOpenObject] = useState(false);
+    const { type, description, title, pattern, enum: _enum } = properties[key_value];
+    const object_toggle = is_open_object ? styles.objectOpen : '';
+    const value = properties[key_value];
+    console.log(value);
+    return (
+        <div>
+            <div className={styles.schemaObjectContent}>
+                <div>
+                    <p><strong>{key_value}</strong></p>
+                </div>
+                <div>
+                    <button onClick={() => setIsOpenObject(!is_open_object)}>{title ? key_value : "object"}</button>
+                </div>
+            </div>
+            <p className={styles.schemaBodyDescription}>{description}</p>
+            <div className={`${type === "object" ? `${styles.schemaObjectBody}` : ''} ${object_toggle}`}>
+                <p><strong>{key_value}</strong></p>
+                <p className={styles.schemaObjectDescription}>{description}</p>
+                <RecursiveProperties properties={value.properties} value={key_value}/>
+            </div>
+        </div>
+    )
+}
