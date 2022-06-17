f46aaf41a445386b21080d58dfa3716313fbc904
Sanja Chhatwal <sanjam@deriv.com>
2022-06-17 12:14:03 +0400

    Fixed the type errors

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,45 +1,31 @@
-import React from "react";
-import styles from "./Checkbox.module.scss";
+import React from 'react';
+import styles from './Checkbox.module.scss';
 
-interface Checkbox {
-  name: string;
-  id: string;
-  label: string;
-  className : string;
-  value : string;
-  onClickFunction : any;
+interface CheckboxProps {
+    id: string;
+    label: string;
+    className: string;
+    value: string;
+    onClickFunction: any;
+    name?: JSX.Element | JSX.Element[];
 }
 
-export default function Checkbox({
-  name ,
-  id ,
-  label ,
-  className ,
-  value,
-  onClickFunction 
-}: Checkbox) {
-  const [is_checked, setIsChecked] = React.useState(false);
-  const checkbox_active = is_checked ? " " + styles.activeCheckbox : "";
-  const new_class_name = className !== "" ? " " + className : "";
-  const toggleCheckbox = () =>
-    is_checked ? setIsChecked(false) : setIsChecked(true);
-  return (
-    <div>
-      <input
-        {...name}
-        id={id}
-        type="checkbox"
-        defaultValue={value}
-        defaultChecked={is_checked}
-      />
-      <span
-        className={`${styles.customCheckbox}${checkbox_active}${new_class_name}`}
-        onClick={() => {
-          if (onClickFunction !== null) onClickFunction();
-          toggleCheckbox();
-        }}
-      />
-      <label htmlFor={id}>{label}</label>
-    </div>
-  );
+export default function Checkbox({ id, label, className, value, onClickFunction, name }: CheckboxProps) {
+    const [is_checked, setIsChecked] = React.useState(false);
+    const checkbox_active = is_checked ? ' ' + styles.activeCheckbox : '';
+    const new_class_name = className !== '' ? ' ' + className : '';
+    const toggleCheckbox = () => (is_checked ? setIsChecked(false) : setIsChecked(true));
+    return (
+        <div>
+            <input {...name} id={id} type='checkbox' defaultValue={value} defaultChecked={is_checked} />
+            <span
+                className={`${styles.customCheckbox}${checkbox_active}${new_class_name}`}
+                onClick={() => {
+                    if (onClickFunction !== null) onClickFunction();
+                    toggleCheckbox();
+                }}
+            />
+            <label htmlFor={id}>{label}</label>
+        </div>
+    );
 }
