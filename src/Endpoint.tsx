45e9942e4c23ed4a3f2f3a93dba680df082d7ded
Sanja Chhatwal <sanjam@deriv.com>
2022-06-28 15:46:59 +0400

    Endpoint configuration

---
+++ src/Endpoint.tsx
@@ -0,0 +1,96 @@
+import { app_id, server_url } from './storageSignals';
+import styles from './EndPoint.module.scss';
+import { useForm } from 'react-hook-form';
+
+interface EndpointData {
+    app_id: number;
+    server_url: string;
+}
+
+export default function EndPoint() {
+    console.log('Value: ', app_id(), server_url());
+    const {
+        register,
+        formState: { errors },
+        setValue,
+        handleSubmit,
+    } = useForm<EndpointData>();
+
+    const params = new URLSearchParams(window.location.search);
+
+    setValue('app_id', app_id());
+    setValue('server_url', server_url());
+
+    return (
+        <>
+            <form>
+                <div className={styles.pageContent}>
+                    <div className={styles.header}>Change API endpoint</div>
+                    <div className={styles.content}>
+                        <div className={styles.customTextInput} id='custom-text-input'>
+                            <div className={styles.inlineLabel}>Server_URL</div>
+                            <input
+                                {...register('server_url', {
+                                    required: {
+                                        value: true,
+                                        message: 'Server is Required',
+                                    },
+                                })}
+                                name='server_url'
+                                defaultValue={server_url}
+                                placeholder='e.g. frontend.binaryws.com'
+                                className={styles.textInput}
+                                required
+                            />
+                            {errors.server_url && (
+                                <span className={styles.errorMessage}>{errors.server_url.message}</span>
+                            )}
+                            <div className={styles.inlineLabel}>App_id</div>
+                            <input
+                                {...register('app_id', {
+                                    required: {
+                                        value: true,
+                                        message: 'App ID is required',
+                                    },
+                                    pattern: {
+                                        value: /^(0|[1-9]\d*)(\.\d+)?$/,
+                                        message: 'Only Numbers Allowed',
+                                    },
+                                })}
+                                name='app_id'
+                                className={styles.textInput}
+                                defaultValue={app_id}
+                                placeholder='e.g. 9999'
+                                onClick={() => {
+                                    if (server_url) localStorage.setItem('server_url', server_url);
+                                    if (app_id && !isNaN(app_id)) localStorage.setItem('app_id', app_id);
+                                }}
+                                required
+                            />
+                        </div>
+                        {errors.app_id && <span className={styles.errorMessage}>{errors.app_id.message}</span>}
+
+                        <div className={styles.buttons}>
+                            <button type='submit' className={styles.submitButton}>
+                                Submit
+                            </button>
+                            <span style={{ marginLeft: '1.6rem' }} />
+                            <button
+                                type='button'
+                                onClick={() => {
+                                    localStorage.removeItem('app_id');
+                                    localStorage.removeItem('server_url');
+                                    window.history.replaceState({}, document.title, window.location.pathname);
+                                    window.location.reload();
+                                }}
+                                className={styles.resetButton}
+                            >
+                                Reset to original settings
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </>
+    );
+}
--- src/appRegistrationEffects.js
+++ src/appRegistrationEffects.js
@@ -1,28 +1,26 @@
-import { createEffect, createSignal } from "solid-js";
+import { createEffect, createSignal } from 'solid-js';
 
-import { app_id, server_url, token1 } from "./storageSignals";
-import { send } from "./stateSignal";
+import { app_id, server_url, token1 } from './storageSignals';
+import { send } from './stateSignal';
 
-export const [oauthUrl, setOauthUrl] = createSignal("");
+export const [oauthUrl, setOauthUrl] = createSignal('');
 
 createEffect(() => {
-  const is_production = window.location.hostname === "api.deriv.com";
-  const production_app_id = 31063;
-  if (is_production) {
-    localStorage.setItem("app_id", production_app_id);
-    localStorage.setItem("server_url", "https://green.binaryws.com");
-  }
-  const loginUrl = () => {
-    if (is_production) {
-      return `https://oauth.deriv.com/oauth2/authorize?app_id=${production_app_id}&l=EN&brand=deriv`;
+    const is_production = window.location.hostname === 'api.deriv.com';
+    const production_app_id = 31063;
+    localStorage.setItem('app_id', production_app_id);
+    localStorage.setItem('server_url', 'https://green.binaryws.com');
+    const loginUrl = () => {
+        if (is_production) {
+            return `https://oauth.deriv.com/oauth2/authorize?app_id=${production_app_id}&l=EN&brand=deriv`;
+        }
+        if (server_url()) {
+            return `https://${server_url()}/oauth2/authorize?app_id=${app_id()}&l=EN&brand=deriv`;
+        }
+        return `https://oauth.deriv.com/oauth2/authorize?app_id=${app_id()}&l=EN&brand=deriv`;
+    };
+    setOauthUrl(loginUrl());
+    if (token1()) {
+        send('LOGIN');
     }
-    if (server_url()) {
-      return `https://${server_url()}/oauth2/authorize?app_id=${app_id()}&l=EN&brand=deriv`;
-    }
-    return `https://oauth.deriv.com/oauth2/authorize?app_id=${app_id()}&l=EN&brand=deriv`;
-  };
-  setOauthUrl(loginUrl());
-  if (token1()) {
-    send("LOGIN");
-  }
 });
