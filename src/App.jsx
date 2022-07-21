bd2926109bf216ebc3e5ed07cc76c5477c323870
Sanja Chhatwal <sanjam@deriv.com>
2022-07-21 16:39:44 +0400

    Reverted Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './stateSignal';
 import './resizeEffect';
+import './storageSignals';
 import { useRoutes } from 'react-router-dom';
 import { Suspense } from 'react';
 import Header from './components/Header/Header';
