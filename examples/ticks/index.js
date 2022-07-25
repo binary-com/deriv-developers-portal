e8b3388b8b0f890cf3ce4d9811b4d819ad77ae69
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2022-07-25 12:06:06 +0200

    revert ticks changes

--- examples/ticks/index.js
+++ examples/ticks/index.js
@@ -1,36 +1,25 @@
-import DerivAPIBasic from "https://cdn.skypack.dev/@deriv/deriv-api/dist/DerivAPIBasic";
-
+import DerivAPIBasic from "https://cdn.skypack.dev/@deriv/deriv-api";
 const app_id = 1089; // Replace with your app_id or leave as 1089 for testing.
 const connection = new WebSocket(
   `wss://ws.binaryws.com/websockets/v3?app_id=${app_id}`
 );
 const api = new DerivAPIBasic({ connection });
-const tickStream = () => api.subscribe({ ticks: "R_100" });
 
-const tickResponse = async (res) => {
+const tickResponse = (res) => {
   const data = JSON.parse(res.data);
-  if (data.error !== undefined) {
-    console.log("Error : %o", data.error.message);
+  console.log(data.tick);
+
+  // After 15 seconds, will stop tick response.
+  setTimeout(async () => {
     connection.removeEventListener("message", tickResponse, false);
     await api.disconnect();
-  }
-  if (data.msg_type === "tick") {
-    console.log(data.tick);
-  }
+  }, 15000);
 };
 
-const subscribeTicks = async () => {
-  await tickStream();
+const ticks = async () => {
+  await api.ticks("R_100");
   connection.addEventListener("message", tickResponse);
 };
 
-const unsubscribeTicks = async () => {
-  connection.removeEventListener("message", tickResponse, false);
-  tickStream().unsubscribe();
-};
-
-const ticks_button = document.querySelector("#ticks");
-ticks_button.addEventListener("click", subscribeTicks);
-
-const unsubscribe_ticks_button = document.querySelector("#ticks-unsubscribe");
-unsubscribe_ticks_button.addEventListener("click", unsubscribeTicks);
+const button = document.getElementById("ticks");
+button.addEventListener("click", ticks);
