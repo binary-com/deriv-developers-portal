6f82e9a7fe611921882a5f56638dda911e835c60
hubert-deriv <80688204+hubert-deriv@users.noreply.github.com>
2022-08-04 17:06:25 +0200

    Update index.js

--- examples/profit_table/index.js
+++ examples/profit_table/index.js
@@ -1,6 +1,6 @@
 import DerivAPIBasic from "https://cdn.skypack.dev/@deriv/deriv-api/dist/DerivAPIBasic";
 
-const app_id = 32449; // Replace with your app_id or leave as 1089 for testing.
+const app_id = 32450; // Replace with your app_id or leave as 1089 for testing.
 const connection = new WebSocket(
   `wss://ws.binaryws.com/websockets/v3?app_id=${app_id}`
 );
