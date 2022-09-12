928f78494df6762395286ccf87e2c1666837df65
sanjam chhatwal <sanjam@deriv.com>
2022-09-13 02:15:00 +0400

    Added Form validation to Endpoint Page in Deriv API

--- src/Endpoint.tsx
+++ src/Endpoint.tsx
@@ -4,46 +4,54 @@ import { useRef } from 'react';
 import { stateService } from './stateSignal';
 import { app_id, server_url, setAppId, setServerUrl, socket_url } from './storageSignals';
 
+
+interface FormData {
+    server_url: string,
+    app_id: number,
+    inputAppRef: object,
+    form_app_id:number
+}
+
+
 const EndPoint = () => {
     const {
         register,
         formState: { errors },
-    } = useForm({ mode: 'onSubmit' });
+        handleSubmit
+    } = useForm();
 
-    const inputServerRef = useRef();
-    const inputAppRef = useRef()
     const server_url_ui = server_url();
     const app_id_ui = app_id();
     const socket_url_ui = socket_url();
 
-    const handleClick = () => {
-        const updatedAppId = inputAppRef.current.value;
-        const updatedServerUrl = inputServerRef.current.value;
-        setAppId(updatedAppId);
-        setServerUrl(updatedServerUrl);
+    const handleClick = (data) => {
+        setAppId(data.app_id);
+        setServerUrl(data.server_url);
         stateService.send('LOGOUT');
         sessionStorage.removeItem('token1');
-        location.replace('/docs/app-registration/');
     };
 
     return (
         <>
-            <form onSubmit={handleClick}>
+            <form onSubmit={handleSubmit(handleClick)}>
                 <div className={styles.pageContent}>
                     <div className={styles.header}>Change API endpoint</div>
                     <div className={styles.content}>
                         <div className={styles.customTextInput} id='custom-text-input'>
                             <div className={styles.inlineLabel}>Server_URL</div>
                             <input
-                                {...register('server_url', {
+                                {...register('server_url',{
                                     required: {
                                         value: true,
                                         message: 'Server is Required',
                                     },
+                                    pattern:{
+                                        value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                                        message: 'You have entered an invalid email address!'
+                                    }
                                 })}
                                 name='server_url'
                                 defaultValue={server_url_ui}
-                                ref={inputServerRef}
                                 placeholder='e.g. frontend.binaryws.com'
                                 className={styles.textInput}
                                 required
@@ -53,7 +61,7 @@ const EndPoint = () => {
                             )}
                             <div className={styles.inlineLabel}>App_id</div>
                             <input
-                                {...register('app_id', {
+                                {...register('app_id',{
                                     required: {
                                         value: true,
                                         message: 'App ID is required',
@@ -65,14 +73,12 @@ const EndPoint = () => {
                                 })}
                                 name='app_id'
                                 className={styles.textInput}
-                                id='app_id'
                                 defaultValue={app_id_ui}
                                 placeholder='e.g. 9999'
-                                ref={inputAppRef}
                                 required
                             />
+                            {errors.app_id && <span className={styles.errorMessage}>{errors.app_id?.message}</span>}
                         </div>
-                        {errors.app_id && <span className={styles.errorMessage}>{errors.app_id?.message}</span>}
                         <div className={styles.url}>
                             <span className={styles.urlLabel}>Connected to :</span>{' '}
                             <div className={styles.urlId}> {socket_url_ui}</div>
