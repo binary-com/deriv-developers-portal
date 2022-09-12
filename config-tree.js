7d90f4243dfddc6c595bdc02e03ceddb54578abf
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-09-12 11:56:29 +0200

    generate JSON data concept

---
+++ config-tree.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const dirTree = require("directory-tree");
+
+const tree = dirTree('./config/v3', {extensions:/\.json$/});
+
+fs.writeFile('./config/config-tree.json', JSON.stringify(tree, null, 2), function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+});
