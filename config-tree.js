cd6c278507c1b8ddab478d7059ed9ad139ce7386
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-09-14 16:31:22 +0200

    removing files

--- config-tree.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const dirTree = require("directory-tree");
-
-const tree = dirTree('./config/v3', {extensions:/\.json$/});
-
-fs.writeFile('./api-data/config-tree.json', JSON.stringify(tree, null, 2), function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-});
