267e933f27fe200633acc452790d8c2c80b46bc5
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-09-30 16:55:34 +0200

    IT WORKSgit add .

--- src/routes-data/sidebar-routes.jsx
+++ src/routes-data/sidebar-routes.jsx
@@ -8,9 +8,26 @@ export const getting_started = [
         label: "Implement Now",
         children: [
             {
-                path: 'ticks',
+                path: 'child',
                 element: <div>This is a child of getting started</div>,
-                label: "Ticks",
+                label: "child",
+                children: [
+                   {
+                    path: 'child-child',
+                    element: <div>This is a child of getting started</div>,
+                    label: "child-child1",
+                   } 
+                ]
+            },
+            {
+                path: 'child2',
+                element: <div>This is a child of getting started</div>,
+                label: "child2",
+            },
+            {
+                path: 'child3',
+                element: <div>This is a child of getting started</div>,
+                label: "child3",
             }
         ]
     },
@@ -31,30 +48,29 @@ const what_can_you_do = [];
 const resources = [];
 const rest = [];
 
-function LinkComponent({route, children}) {
-    const child_links = Object.entries(children).map(items => {
-        const has_children = items.children;
-        const route = items[1];
-            return (
-                <div key={route.label}>
-                    <Link to={route.path}>{route.label}</Link>
-                    { has_children && <LinkComponent route={children} children={has_children} /> }
-                </div>
-            )
-    })
-    return child_links;
+function LinkComponent({route}) {
+    return (
+        <div key={route.label}>
+            <Link to={route.path}>{route.label}</Link>
+            { route.children && route.children.map(child => {
+                return (
+                    <div className="menu-child"> 
+                        <LinkComponent route={child} /> 
+                    </div>
+                )})
+            }
+        </div>
+    )
 }
 
 export const SidebarMenuItems = ({routes}) => {
-    let menu_items = [];
     const routes_array = Object.values(routes);
     // console.log(...routes_array);
     const map_links = Object.entries(routes_array).map(items => {
         const route = items[1];
-        const has_children = route.children;
         return (
             <>
-                <LinkComponent route={route} children={has_children} />
+                <LinkComponent route={route} />
             </>
         )
     })
