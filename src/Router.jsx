1cd0a05b5417161fe5449a9cc294aa0f7ed8fa52
sanjam chhatwal <sanjam@deriv.com>
2022-10-06 11:06:50 +0400

    Ran prettier

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { sidebar_routes } from './routes-data/sidebar-routes';
 const Endpoint = React.lazy(() => import('./components/endpoint/Endpoint/Endpoint'));
 const HomePage = React.lazy(() => import('./components/homepage/Homepage/Homepage'));
-const Docs = React.lazy(() => import('./components/docs/Docs/Docs'));    
+const Docs = React.lazy(() => import('./components/docs/Docs/Docs'));
 const ApiExplorer = React.lazy(() => import('./components/playground/ApiExplorer/ApiExplorer'));
 const AppRegistration = React.lazy(() => import('./components/app-registration/AppRegistration/AppRegistration'));
 const ApiGuide = React.lazy(() => import('./components/api-guide/ApiGuide/ApiGuide'));
@@ -10,7 +10,6 @@ const Faq = React.lazy(() => import('./components/faq/Faq/Faq'));
 const Json = React.lazy(() => import('./components/json-schemas/JsonSchemas'));
 const BugBounty = React.lazy(() => import('./components/bounty/Bounty/Bounty'));
 const Quickstart = React.lazy(() => import('./components/quickstart/Quickstart/Quickstart'));
-
 
 export const routes = [
     {
@@ -24,33 +23,33 @@ export const routes = [
             {
                 path: '',
                 element: <Quickstart />,
-                label: "Quickstart",
+                label: 'Quickstart',
             },
             ...sidebar_routes,
             {
                 path: 'api-explorer',
                 element: <ApiExplorer />,
-                label: "Api explorer",
+                label: 'Api explorer',
             },
             {
                 path: 'api-guide',
                 element: <ApiGuide />,
-                label: "Api guide",
+                label: 'Api guide',
             },
             {
                 path: 'faq',
                 element: <Faq />,
-                label: "FAQ",
+                label: 'FAQ',
             },
             {
                 path: 'json-schemas',
                 element: <Json />,
-                label: "JSON Schemas",
+                label: 'JSON Schemas',
             },
             {
                 path: 'bug-bounty',
                 element: <BugBounty />,
-                Label: "Bug bounty",
+                Label: 'Bug bounty',
             },
         ],
     },
@@ -58,7 +57,7 @@ export const routes = [
     {
         path: 'app-registration',
         element: <AppRegistration />,
-        label: "App registration",
+        label: 'App registration',
     },
     {
         path: 'api-explorer',
--- src/components/docs/Docs/Sidebar/Sidebar.jsx
+++ src/components/docs/Docs/Sidebar/Sidebar.jsx
@@ -1,37 +1,37 @@
-import styles from "./Sidebar.module.scss";
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import { getting_started, resources, SidebarMenuItems, sidebar_routes,what_can_you_do, implement_now } from "../../../../routes-data/sidebar-routes";
+import styles from './Sidebar.module.scss';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import {
+    getting_started,
+    resources,
+    SidebarMenuItems,
+    sidebar_routes,
+    what_can_you_do,
+    implement_now,
+} from '../../../../routes-data/sidebar-routes';
 
 const Sidebar = () => {
+    const location = useLocation();
 
-  const location = useLocation();
-
-  return (
-    <div className={styles.sidebarleft} data-id="sidebarleft">
-      <p className={styles.sidebartitle}>Documentation</p>
-      <div>
-        {[
-          ["/docs/", "Quickstart"],
-        ].map(([to, label]) => {
-          return (
-            <div key={to}>
-              <Link
-                to={to}
-                className={to === location.pathname ? styles.selected : ""}
-                data-id={to}
-              >
-                {label}
-              </Link>
+    return (
+        <div className={styles.sidebarleft} data-id='sidebarleft'>
+            <p className={styles.sidebartitle}>Documentation</p>
+            <div>
+                {[['/docs/', 'Quickstart']].map(([to, label]) => {
+                    return (
+                        <div key={to}>
+                            <Link to={to} className={to === location.pathname ? styles.selected : ''} data-id={to}>
+                                {label}
+                            </Link>
+                        </div>
+                    );
+                })}
+                <SidebarMenuItems routes={getting_started} />
+                <SidebarMenuItems routes={what_can_you_do} />
+                <SidebarMenuItems routes={resources} />
             </div>
-          );
-        })}
-        <SidebarMenuItems routes={getting_started}/>
-        <SidebarMenuItems routes={what_can_you_do}/>
-        <SidebarMenuItems routes={resources}/>
-      </div>
-    </div>
-  );  
+        </div>
+    );
 };
 
 export default Sidebar;
