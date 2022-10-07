df0e1bc5b542bbb18cd98a957a97165de8ae4b33
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-10-07 13:44:55 +0200

    implementing new quickstart

--- src/components/quickstart/Quickstart/Quickstart.jsx
+++ src/components/quickstart/Quickstart/Quickstart.jsx
@@ -3,54 +3,16 @@ import { SandboxIframe, sandboxes } from '../../utility/SandboxIframe/SandboxIfr
 
 const QuickStart = () => (
   <div className="page-content">
-    <h1 className={styles.doc_main_title}>Quickstart to Deriv API</h1>
+    <h3>Quickstart</h3>
     <div>
       <p>
-        On this page, you&#39;ll find code samples in various programming
-        languages showing you how to work with the Deriv API to perform some of
-        the most important operations.
+      The easiest way to understand how Deriv API works is to try our Tick call:
       </p>
-      <p>
-          You can find all of the other available calls in the <a href='/api-explorer/'>API Explorer</a>.
-      </p>
-      <h3 className={styles.api_sub_title}>Before you begin</h3>
-      <ul className="bullet">
-        <li>
-          Open a <a href="https://deriv.com/">Deriv account</a> (either a demo
-          or real account).
-        </li>
-        <li>
-          Create a new token using the{" "}
-          <a href="https://app.deriv.com/account/api-token">admin scope</a>.
-        </li>
-        <li>
-          Register your app to receive your <b>app_id</b> or use{" "}
-          <b>app_id 1089</b> to test Deriv API.
-        </li>
-      </ul>
-      <h3 className={styles.api_sub_title}>Setting up your environment</h3>
-      <p>
-        Instructions for setting up your environment and running the examples in
-        your desired programming language are given as comments in the code
-        samples.
-      </p>
+      <SandboxIframe sandbox={sandboxes.ticks} />
+      <p>We'll start with the ticks example because it's a pretty simple block of code to implement.</p>
+      <p>A tick is a measure of minimum upward or downward movement in the price of a trading commodity. We subscribe to the ticks functionality from the Deriv API websocket, which will return new tick data every second. As an example you could create an interactive tick graph with this data.</p>
+      <p>You can find other available calls in the <a>Implement now</a> or in the <a href="https://api.deriv.com/api-explorer/">API Explorer.</a></p>
     </div>
-    <h3>Here is how our API works</h3>
-    <p>
-      Below, you see a code example on retrieving <b>tick data</b> from the API. A tick is a measure of minimum upward or downward movement in the price of
-      a trading commodity. This example shows you how to collect ticks for your
-      trading app using Deriv's API.
-    </p>
-    <p>
-      We start off with an example of ticks, because it is a fairly simple block of code to implement.
-      But, if you plan to develop for example a trading application, it is fundamentally also an important feature.
-    </p>
-    <p>
-      We subscribe to the ticks functionality from the Deriv API websocket, which will return new tick data every second or so.
-      With this data you could for example create an interactive tick graph.
-    </p>
-    <h3>Ticks API call example</h3>
-    <SandboxIframe sandbox={sandboxes.ticks} />
   </div>
 );
 
