5ff561f980130a61c8afc22a69fcfd7dfbf9f0bf
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-10-10 12:31:40 +0200

    fixing staging

--- src/components/docs/Docs/Sidebar/Sidebar.jsx
+++ src/components/docs/Docs/Sidebar/Sidebar.jsx
@@ -1,28 +1,38 @@
 import styles from './Sidebar.module.scss';
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import {
-    getting_started,
-    resources,
-    SidebarMenuItems,
-    what_can_you_do,
-    rest,
-} from '../../../../routes-data/sidebar-routes';
 
 const Sidebar = () => {
     const location = useLocation();
 
     return (
-        <div className={styles.sidebarleft} data-id='sidebarleft'>
-            <p className={styles.sidebartitle}>Documentation</p>
-            <div>
-                <SidebarMenuItems routes={rest} />
-                <SidebarMenuItems routes={getting_started} />
-                <SidebarMenuItems routes={what_can_you_do} />
-                <SidebarMenuItems routes={resources} />
+        <div className={styles.sidebarleft} data-id="sidebarleft">
+          <p className={styles.sidebartitle}>Deriv API</p>
+          <div>
+            {[
+              ["/docs/", "Quickstart"],
+              ["/docs/app-registration/", "App registration"],
+              ["/api-explorer/", "API explorer"],
+              ["/docs/api-guide/", "API guide"],
+              ["/docs/faq/", "FAQ"],
+              ["/docs/json-schemas/", "JSON Schemas"],
+              ["/docs/bug-bounty/", "Bug Bounty"],
+            ].map(([to, label]) => {
+                return (
+                    <div key={to}>
+                        <Link
+                            to={to}
+                            className={to === location.pathname ? styles.selected : ""}
+                            data-id={to}
+                        >
+                            {label}
+                        </Link>
+                    </div>
+                );
+            })}
             </div>
         </div>
-    );
+  );
 };
 
 export default Sidebar;
