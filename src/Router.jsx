53dca087063864e4551195a2240a9f099a2c214f
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-10-11 16:45:50 +0200

    fixing styling issues

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { sidebar_routes } from './routes-data/sidebar-routes';
 const Endpoint = React.lazy(() => import('./components/endpoint/Endpoint/Endpoint'));
 const HomePage = React.lazy(() => import('./components/homepage/Homepage/Homepage'));
-const Docs = React.lazy(() => import('./components/docs/Docs/Docs'));
 const ApiExplorer = React.lazy(() => import('./components/playground/ApiExplorer/ApiExplorer'));
 const AppRegistration = React.lazy(() => import('./components/app-registration/AppRegistration/AppRegistration'));
 
