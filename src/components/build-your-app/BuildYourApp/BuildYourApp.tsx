9703314bbdba29d116ed609b7ecb3504b43687c3
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-10-14 16:30:24 +0200

    fixing Build your App page

--- src/components/build-your-app/BuildYourApp/BuildYourApp.tsx
+++ src/components/build-your-app/BuildYourApp/BuildYourApp.tsx
@@ -2,7 +2,7 @@ import { oauthUrl } from "../../../global-functions/appRegistrationEffects";
 export default function BuildYourApp() {
     return (
         <div>
-            <h3>Before you begin</h3>
+            <h3>Build your app</h3>
             <div>
                 <h5>1. Open a Deriv account</h5>
                 <p><a href="https://deriv.com/signup">Sign up</a> for a Deriv account (either a demo or a real account) at no charge.</p>
@@ -12,7 +12,7 @@ export default function BuildYourApp() {
                 <p>To gain access to the Deriv API, you'll need an API token with the Admin scope. We'll use this token to tag your application to your Deriv account.</p>
                 <p>Follow these steps to create an API token:</p>
                 <ul className="numbered-list">
-                    <li><a href={oauthUrl()}>Log in</a> to your Deriv account and go to <a href="https://app.deriv.com/account/api-token">Settings &gt; API token.</a></li>
+                    <li><a href={oauthUrl()}>Log in</a> to your Deriv account and go to <a href="https://app.deriv.com/account/api-token">Manage account settings &gt; API token.</a></li>
                     <li>Select the <strong>Admin</strong> scope, enter a name for your token, and click <strong>Create</strong>.</li>
                     <li>Your API token will appear in the list at the bottom of the screen. You'll need it for the next step.</li>
                 </ul>
@@ -22,7 +22,7 @@ export default function BuildYourApp() {
                 <p>You'll get your app ID when you register your application. With this app ID and the API token from the previous step, your application can talk to the Deriv API. Also, whenever you need support, just quote your app ID when talking to our Customer Support team.</p>
                 <p>Follow these steps to register your application:</p>
                 <ul className="numbered-list">
-                    <li><a href={oauthUrl()}>Log in</a> to your Deriv account and <a href="https://api.deriv.com/app-registration/">complete this form</a>. Remember the API token you created during the previous step? You'll need it in this step.</li>
+                    <li><a href={oauthUrl()}>Log in</a> to your Deriv account and <a href="/app-registration">complete this form</a>. Remember the API token you created during the previous step? You'll need it in this step.</li>
                     <li>After your application is registered, it will appear in the <strong>Manage existing applications</strong> tab. You may now delete your API token.</li>
                 </ul>
             </div>
