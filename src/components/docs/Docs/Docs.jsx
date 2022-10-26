f03db72e76006c7adff4aed345a4f9acf732bd1e
sanjam chhatwal <sanjam@deriv.com>
2022-10-26 11:58:15 +0400

    Removed Selector and Deleted redundant files

--- src/components/docs/Docs/Docs.jsx
+++ src/components/docs/Docs/Docs.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
 import Sidebar from "./Sidebar/Sidebar";
 import "../../../index.scss";
-import MobilePageSelectLazy from "./MobilePageSelect/MobilePageSelectLazy";
 import DelayedFallback from "../../global/DelayedFallback/DelayedFallback";
 
 function Docs() {
@@ -10,7 +9,6 @@ function Docs() {
     <div id="content" className="doc-content">
       <Sidebar />
       <div className="pageWrapper">
-        <MobilePageSelectLazy />
         <Suspense fallback={<DelayedFallback />}>
           <Outlet />
         </Suspense>
