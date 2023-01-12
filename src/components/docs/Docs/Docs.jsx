3f28b9229e9ab08ef8cc49f5140a47eb4ad66f6a
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2023-01-12 16:05:36 +0100

    chore: removing redundant code

--- src/components/docs/Docs/Docs.jsx
+++ src/components/docs/Docs/Docs.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { Suspense } from 'react';
-import { RenderOfficialDomainContents } from '../../utility/RenderOfficialDomainContents/RenderOfficialDomainContents';
-import QuickStart from '../../quickstart/Quickstart/Quickstart';
 import Sidebar from './Sidebar/Sidebar';
 import '../../../index.scss';
 import DelayedFallback from '../../global/DelayedFallback/DelayedFallback';
 
 function Docs() {
-    const location = useLocation();
-    const [is_quickstart, setIsQuickstart] = React.useState(false);
-
-    React.useEffect(() => {
-        const current_path = location.pathname;
-        if (current_path === '/docs') {
-            setIsQuickstart(true);
-        } else {
-            setIsQuickstart(false);
-        }
-    }, []);
-
     return (
         <div id='content' className='doc-content'>
             <Sidebar />
